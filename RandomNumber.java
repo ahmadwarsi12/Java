5b2ce233de22190dc457a75c931707ec7be95348
Ahmad Ali Warsi <111039664+ahmadwarsi12@users.noreply.github.com>
2023-09-30 20:57:51 +0530

    Add files via upload

---
+++ RandomNumber.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomNumber { 
+    public static void main(String[] args){
+
+        int answer, guess;
+        final int MAX = 100;
+
+        Scanner sc =  new Scanner(System.in);
+        Random rand = new Random();
+
+        answer = rand.nextInt(MAX) + 1;
+
+        System.out.print("Guess a number between 1 and 100: ");
+        guess = sc.nextInt();
+
+        if(guess == answer){
+            System.out.println("Good job, the number was " + answer);
+        }
+        else{
+            System.out.println("Sorry, but the number was " + answer);
+        }
+    }
+}
